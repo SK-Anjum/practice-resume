1563907256a43a1a1c50b6c4363c0aed377efdc7
SK-Anjum <161378654+SK-Anjum@users.noreply.github.com>
2024-10-01 00:56:37 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,20 +1,22 @@
-
 import Link from "next/link";
-
-export default function Home() {
-  return (
-   <div>
-
-    <ul className="flex gap-20 bg-blue-300">
-      <li>                      </li>
-      <li> <Link href="/"> Home </Link> </li>
-      <li> <Link href="/about" > About </Link></li>
-      <li> <Link href="/education"> Education</Link></li>
-      <li> <Link href="/skills"> Skills</Link></li>
-    </ul>
-      <h1>  RESUME</h1> 
-    
-    </div>
-
-  );
+export default function skills(){
+    return (
+       <div>
+       <ul className="flex gap-20 bg-blue-300">
+        <li>                      </li>
+        <li> <Link href="/"> Home </Link> </li>
+        <li> <Link href="/about" > about </Link></li>
+        <li> <Link href="/education"> Education</Link></li>
+        <li> <Link href="/skills"> Skills</Link></li>
+</ul>
+<p> 
+    <ol>
+        <li> Time management</li>
+        <li> Hard working</li>
+        <li> cooperative</li>
+        <li> Team Lead</li>
+    </ol>
+</p>
+</div>
+    );
 }
